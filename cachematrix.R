f9858dfc7a584d129378885f7a66943a68cf46a1
RasoolRah <123964430+RasoolRah@users.noreply.github.com>
2023-01-30 17:58:40 +0330

    my assignment

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,28 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+##my code
+cachemean <- function(x, ...) {
+  m <- NULL
+  makeCacheMatrix <- function() {
+    m <<- mean(x, ...)
+    m
+  }
+  cacheSolve <- function(y = NULL) {
+    ## Return the mean if y is NULL
+    if (is.null(y)) {
+      ## Return the mean if it is already calculated
+      if (!is.null(m)) {
+        m
+      } else {
+        makeCacheMatrix()
+      }
+    } else {
+      x <<- y
+      m <<- NULL
+    }
+  }
+  list(get = cacheSolve, set = cacheSolve)
+}
